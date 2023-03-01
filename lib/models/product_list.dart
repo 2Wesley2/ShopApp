88bebcfa1ff1d85bcca12ce89760f389ea4f0a76
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-03-01 16:18:52 -0300

    Retira a lógica da criação de um novo produto da tela e poe na camada de modelo, fazendo apenas a chamada do método na tela quando o usuário submete as informções do formulário

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../data/dummy_data.dart';
 import '../models/product.dart';
@@ -11,6 +13,18 @@ class ProductList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  void addProductFromData(Map<String, Object> data) {
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+
+    addProduct(newProduct);
   }
 
   void addProduct(Product product) {
