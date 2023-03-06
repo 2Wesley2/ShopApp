b93982d7a092f13b2e4a6c07f8e0003255b9555a
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-03-06 13:57:42 -0300

    Exibe tela de loading ao adicionar produto e em seguida após resposta do firebase é mostrada a tela de produtos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -18,7 +18,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     final future = http.post(
       Uri.parse('$_baseUrl/products.json'),
       body: jsonEncode({
@@ -29,7 +29,7 @@ class ProductList with ChangeNotifier {
         "isFavorite": product.isFavorite,
       }),
     );
-    future.then(
+    return future.then<void>(
       (response) {
         final id = jsonDecode(response.body)['name'];
         _items.add(Product(
@@ -45,13 +45,14 @@ class ProductList with ChangeNotifier {
     );
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+    return Future.value();
   }
 
   void removeProduct(Product product) {
@@ -64,7 +65,7 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final newProduct = Product(
@@ -76,9 +77,9 @@ class ProductList with ChangeNotifier {
     );
 
     if (hasId) {
-      updateProduct(newProduct);
+      return updateProduct(newProduct);
     } else {
-      addProduct(newProduct);
+      return addProduct(newProduct);
     }
   }
 }
