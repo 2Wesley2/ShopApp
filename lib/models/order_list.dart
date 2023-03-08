04f6d16f49fb31ff592f1746e5c84d885c18d9a4
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-03-08 12:09:41 -0300

    Obtendo ordens de compra do firebase e renderizando na tela

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item_model.dart';
 import 'package:shop/models/order.dart';
-import '../utils/constants.dart';
+import 'package:shop/utils/constants.dart';
 
 class OrderList with ChangeNotifier {
   final List<Order> _items = [];
@@ -12,6 +13,35 @@ class OrderList with ChangeNotifier {
 
   int get itemsCount => _items.length;
   final _baseUrl = Constants.orderBaseUrl;
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.orderBaseUrl}.json'),
+    );
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItemModel(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+    notifyListeners();
+  }
+
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
