c58f2c6f061b47dd1179b053dfa8856cb2e000b7
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-03-08 11:27:03 -0300

    Persistindo dados da ordem de compra no firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,8 +1,9 @@
-import 'dart:math';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order.dart';
+import '../utils/constants.dart';
 
 class OrderList with ChangeNotifier {
   final List<Order> _items = [];
@@ -10,15 +11,37 @@ class OrderList with ChangeNotifier {
   List<Order> get items => [..._items];
 
   int get itemsCount => _items.length;
-
-  void addOrder(Cart cart) {
+  final _baseUrl = Constants.orderBaseUrl;
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final response = await http.post(
+      Uri.parse('$_baseUrl.json'),
+      body: jsonEncode(
+        {
+          "total": cart.totalAmount,
+          "date": date.toIso8601String(),
+          "products": cart.items.values
+              .map(
+                (cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'name': cartItem.name,
+                  'quantity': cartItem.quantity,
+                  'price': cartItem.price,
+                },
+              )
+              .toList(),
+        },
+      ),
+    );
+    final id = jsonDecode(response.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         products: cart.items.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
     notifyListeners();
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-wfa-default-rtdb.firebaseio.com/product';
+
+  static const orderBaseUrl =
+      'https://shop-wfa-default-rtdb.firebaseio.com/order';
 }
